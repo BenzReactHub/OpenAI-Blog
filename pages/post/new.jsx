38cdc347689914dc9725c988e30bdc62455e615c
Len Rich <lenrich1403@gmail.com>
2024-02-20 13:40:54 +0800

    文章生成加載畫面

--- pages/post/new.jsx
+++ pages/post/new.jsx
@@ -3,53 +3,78 @@ import { useState } from "react";
 import AppLayout from "../../components/AppLayout";
 import { useRouter } from "next/navigation";
 import { getAppProps } from "../../utils/getAppProps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
 export default function NewPost(props) {
   const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [generating, setGenerating] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const response = await fetch("/api/generatePost", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({ topic, keywords }),
-    }).then((res) => res.json());
-    if(response?.postId) {
-      router.push(`/post/${response.postId}`)
+    setGenerating(true);
+    try {
+      const response = await fetch("/api/generatePost", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ topic, keywords }),
+      }).then((res) => res.json());
+      if (response?.postId) {
+        router.push(`/post/${response.postId}`);
+      }
+    } catch (error) {
+      setGenerating(false);
     }
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <strong>Generate a blog post on the topic of:</strong>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            value={topic}
-            onChange={(e) => {
-              setTopic(e.target.value);
-            }}
-          />
+    <div className="h-full overflow-hidden">
+      {!!generating && (
+        <div className="text-green-500 flex h-full animate-pulse w-full flex-col justify-center items-center">
+          <FontAwesomeIcon icon={faBrain} className="text-8xl" />
+          <h6>Generating...</h6>
         </div>
-        <div>
-          <label>
-            <strong>Targeting the following keywords:</strong>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            value={keywords}
-            onChange={(e) => {
-              setKeywords(e.target.value);
-            }}
-          />
+      )}
+
+      {!generating && (
+        <div className="w-full h-full flex flex-col overflow-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+          >
+            <div>
+              <label>
+                <strong>Generate a blog post on the topic of:</strong>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                value={topic}
+                onChange={(e) => {
+                  setTopic(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Targeting the following keywords:</strong>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                value={keywords}
+                onChange={(e) => {
+                  setKeywords(e.target.value);
+                }}
+              />
+              <small className="block mb-2">
+                Separate keyword with a comma
+              </small>
+            </div>
+            <button type="submit" className="btn">
+              Generate
+            </button>
+          </form>
         </div>
-        <button type="submit" className="btn">
-          Generate
-        </button>
-      </form>
+      )}
     </div>
   );
 }
