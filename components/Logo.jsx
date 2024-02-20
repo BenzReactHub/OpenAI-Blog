18e4e450b8bd4c00afb6440bd0f2c5bf427c4756
Len Rich <lenrich1403@gmail.com>
2024-02-20 17:31:54 +0800

    logo text and icon set center

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="text-3xl text-center py-4 flex font-heading items-center gap-2">
+    <div className="text-3xl text-center py-4 flex font-heading justify-center items-center gap-2">
       <span className="inline-block">OpenAI-Blog</span>
       <FontAwesomeIcon icon={faBrain} className="text-2xl text-slate-400" />
     </div>
