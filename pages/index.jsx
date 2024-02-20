24a3ec1740c43a9260f3545071965c0b88ce7861
Len Rich <lenrich1403@gmail.com>
2024-02-20 23:58:04 +0800

    edit form width

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
       <div className="w-screen h-screen bg-gradient-r absolute"></div>
-      <div className="glass flex font-bold w-[50rem] max-w-[80%] flex-col gap-6 items-center relative z-10 px-10 py-5 text-center rounded-md backdrop-blur-sm">
+      <div className="glass flex font-bold w-[80%] lg:w-[35rem] flex-col gap-6 items-center relative z-10 px-10 py-5 text-center rounded-md backdrop-blur-sm">
         <Logo />
         <p>
           Discover the future of content creation at OpenAI-Blog. Our
--- pages/post/new.jsx
+++ pages/post/new.jsx
@@ -41,7 +41,7 @@ export default function NewPost(props) {
         <div className="w-full h-full flex flex-col overflow-auto">
           <form
             onSubmit={handleSubmit}
-            className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+            className="m-auto w-[80%] lg:w-[35rem] max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
           >
             <div>
               <label>
