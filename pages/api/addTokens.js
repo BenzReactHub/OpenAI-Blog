9fc30dc40b955d0fbf64a5300d2f6bfae382b79a
Len Rich <lenrich1403@gmail.com>
2024-02-20 13:23:56 +0800

    串接 stripe webhooks

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,5 +1,4 @@
 import { getSession } from "@auth0/nextjs-auth0";
-import clientPromise from "../../lib/mongodb";
 import stripeInit from 'stripe';
 
 const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
@@ -18,27 +17,18 @@ export default async function handler(req, res) {
   const checkoutSession = await stripe.checkout.sessions.create({
     line_items: lineItems,
     mode: "payment",
-    success_url: `${protocol}${host}/success`
+    success_url: `${protocol}${host}/success`,
+    payment_intent_data: {
+      metadata: {
+        sub: user.sub
+      }
+    },
+    metadata: {
+      sub: user.sub
+    }
   })
 
-  const client = await clientPromise;
-  const db = client.db("OpenAIBlog");
-  const userProfile = await db.collection("users").updateOne(
-    {
-      auth0Id: user.sub,
-    },
-    {
-      $inc: {
-        availableToken: 10,
-      },
-      $setOnInsert: {
-        auth0Id: user.sub,
-      },
-    },
-    {
-      upsert: true,
-    }
-  );
+  
 
   res.status(200).json({ session: checkoutSession });
 }
